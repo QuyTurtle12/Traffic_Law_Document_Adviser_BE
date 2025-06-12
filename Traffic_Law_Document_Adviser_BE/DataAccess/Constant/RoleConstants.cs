0fadb79b1d5ea10093b66962a89ab6695a4b6954
Phan Pham Hoa <hoappse171730@fpt.edu.vn>
2025-06-12 20:55:36 +0700

    fix: change hard delete user into soft-delete + add staff role

--- Traffic_Law_Document_Adviser_BE/DataAccess/Constant/RoleConstants.cs
+++ Traffic_Law_Document_Adviser_BE/DataAccess/Constant/RoleConstants.cs
@@ -1,37 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccess.Constant
+﻿namespace DataAccess.Constant
 {
     public static class RoleConstants
     {
         public const int UserValue = 1;
         public const int ExpertValue = 2;
         public const int AdminValue = 3;
+        public const int StaffValue = 4;            // ← new
 
         public const string User = "User";
         public const string Expert = "Expert";
         public const string Admin = "Admin";
+        public const string Staff = "Staff";         // ← new
 
         public static string ToRoleName(int v) =>
             v switch
             {
                 AdminValue => Admin,
                 ExpertValue => Expert,
-                _ => User,   
+                StaffValue => Staff,           // ← new
+                _ => User,
             };
 
-        public static int ToRoleValue(string name)
-        {
-            return name switch
+        public static int ToRoleValue(string name) =>
+            name switch
             {
                 Admin => AdminValue,
                 Expert => ExpertValue,
+                Staff => StaffValue,           // ← new
                 _ => UserValue
             };
-        }
     }
 }
